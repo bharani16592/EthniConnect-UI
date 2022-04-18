eb5b580192a800d165bcb4b739c1066c74944da4
bharani16592 <bkalagotla@oakland.edu>
2022-04-17 23:05:21 -0400

    enthni connect final working code

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { SignInDataService } from 'src/network/dataServices/SigninDataService';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm = new FormGroup({
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required]),
+  });
+
+  constructor(private _SignInDataService:SignInDataService,
+    private router:Router ) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    this._SignInDataService.queryTheServer({
+      email:this.loginForm.value.email,
+      password:this.loginForm.value.password
+    }).subscribe({
+      
+      next: resp=>{
+        const data:any=resp
+        localStorage.setItem('loginId',data.loginId)
+        localStorage.setItem('isChef',data.chef)
+        if (this._SignInDataService.redirectUrl){
+          this.router.navigate([this._SignInDataService.redirectUrl])
+          return
+        }
+        if(data.chef)
+        {
+          console.log(data.chef)
+          if(!data.chefProfile){
+          console.log(data.chef)
+          console.log(data.chefProfile)
+          this.router.navigate(['/chefprofilesetup'])
+          return
+        }
+        console.log(data.chef)
+        console.log(data.chefProfile)
+        this.router.navigate(['/createmenu'])
+      }
+        else{
+          this.router.navigate(['/EnterZipCode'])
+        }
+        
+      } ,
+      error: err=>{
+        console.log(err)
+        //alert("User has not registered yet. Please signup")
+        alert(err.error.errormessage)
+      }
+   });
+  }
+
+}
